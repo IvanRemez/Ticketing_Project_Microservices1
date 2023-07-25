e8e214c9decb1e93622cab61aa7fa06175bf8862
Ivan_Cydeo <remezivan1@gmail.com>
2023-07-25 15:03:56 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/TicketingProjectMicroservices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMicroservices1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
